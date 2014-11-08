e19776db3e5aab28159e37268bd82f0236ca841e
Mitchell Hentges <mitch9654@gmail.com>
2014-11-08 14:49:52 -0800

    Fixes deprecated function usage Fixes #22

--- main.js
+++ main.js
@@ -7,7 +7,7 @@
  */
 define( function( require, exports, module ) {
 	'use strict';
-	
+
 	// Get dependencies.
 	var Async = brackets.getModule( 'utils/Async' ),
 		Menus = brackets.getModule( 'command/Menus' ),
@@ -17,14 +17,14 @@ define( function( require, exports, module ) {
 		ProjectManager = brackets.getModule( 'project/ProjectManager' ),
 		EditorManager = brackets.getModule( 'editor/EditorManager' ),
 		DocumentManager = brackets.getModule( 'document/DocumentManager' ),
-		PanelManager = brackets.getModule( 'view/PanelManager' ),
+		WorkspaceManager = brackets.getModule( 'view/WorkspaceManager' ),
 		Resizer = brackets.getModule( 'utils/Resizer' ),
 		AppInit = brackets.getModule( 'utils/AppInit' ),
 		FileUtils = brackets.getModule( 'file/FileUtils' ),
 		FileSystem = brackets.getModule( 'filesystem/FileSystem' ),
 		ExtensionUtils = brackets.getModule( 'utils/ExtensionUtils' ),
         NodeDomain = brackets.getModule("utils/NodeDomain"),
-		
+
 		// Extension basics.
 		COMMAND_ID = 'bigeyex.bracketsSFTPUpload.enable',
         COMMAND_ID_UPLOAD = 'bigeyex.bracketsSFTPUpload.upload',
@@ -32,60 +32,60 @@ define( function( require, exports, module ) {
 		Strings = require( 'modules/Strings' ),
         dataStorage = require( 'modules/DataStorageManager' ),
         settingsDialog = require( 'modules/SettingsDialog' ),
-		
+
 		// Preferences.
 		preferences = PreferencesManager.getExtensionPrefs( 'bigeyex.bracketsSFTPUpload' ),
-		
+
 		// Mustache templates.
 		todoPanelTemplate = require( 'text!html/panel.html' ),
 		todoRowTemplate = require( 'text!html/row.html' ),
-		
+
 		// Setup extension.
         serverInfo, //sftp username/password etc;
 		$todoPanel,
         projectUrl,
 		$todoIcon = $( '<a href="#" title="' + Strings.EXTENSION_NAME + '" id="brackets-sftp-upload-icon"></a>' ),
-		
+
 		// Get view menu.
 		menu = Menus.getMenu( Menus.AppMenuBar.VIEW_MENU ),
         contextMenu = Menus.getContextMenu(Menus.ContextMenuIds.PROJECT_MENU);
-	
+
 	// Define preferences.
 	preferences.definePreference( 'enabled', 'boolean', false );
-    
+
     // Get Node module domain
-    var _domainPath = ExtensionUtils.getModulePath(module, "node/SftpUploadDomain"); 
+    var _domainPath = ExtensionUtils.getModulePath(module, "node/SftpUploadDomain");
     var _nodeDomain = new NodeDomain("sftpUpload", _domainPath);
-	
+
 	// Register extension.
     CommandManager.register( Strings.EXTENSION_NAME, COMMAND_ID, togglePanel );
 	CommandManager.register( Strings.UPLOAD_MENU_NAME, COMMAND_ID_UPLOAD, uploadMenuAction );
-    
-    
+
+
 	// Add command to menu.
 	if ( menu !== undefined ) {
 		menu.addMenuDivider();
 		menu.addMenuItem( COMMAND_ID, 'Ctrl-Alt-U' );
 	}
-    
+
     if ( contextMenu !== undefined ) {
         contextMenu.addMenuDivider();
         contextMenu.addMenuItem( COMMAND_ID_UPLOAD );
     }
-	
+
 	// Load stylesheet.
 	ExtensionUtils.loadStyleSheet( module, 'todo.css' );
-	
-	/** 
+
+	/**
 	 * Set state of extension.
 	 */
     // this is a menu item
 	function togglePanel() {
 		var enabled = preferences.get( 'enabled' );
-		
+
 		enablePanel( !enabled );
 	}
-    
+
     function uploadMenuAction(){
         var item = ProjectManager.getSelectedItem();
         var projectUrl = ProjectManager.getProjectRoot().fullPath;
@@ -97,8 +97,8 @@ define( function( require, exports, module ) {
             uploadDirectory(item.fullPath, remotePath);
         }
     }
-	
-	/** 
+
+	/**
 	 * Initialize extension.
 	 */
 	function enablePanel( enabled ) {
@@ -107,25 +107,25 @@ define( function( require, exports, module ) {
 				// Show panel.
 				Resizer.show( $todoPanel );
 			} );
-			
+
 			// Set active class on icon.
 			$todoIcon.addClass( 'active' );
 		} else {
 			// Hide panel.
 			Resizer.hide( $todoPanel );
-			
+
 			// Remove active class from icon.
 			$todoIcon.removeClass( 'active' );
 		}
-		
+
 		// Save enabled state.
 		preferences.set( 'enabled', enabled );
-		preferences.save();
-		
+		preferences.save()
+
 		// Mark menu item as enabled/disabled.
 		CommandManager.get( COMMAND_ID ).setChecked( enabled );
 	}
-	
+
 	// this is called every time the panel opens.
 	function loadSettings( callback ) {
         var changedFiles = dataStorage.get('changed_files');
@@ -137,45 +137,45 @@ define( function( require, exports, module ) {
                 file: filepath.replace(projectUrl, '')
             });
         }
-        
+
         $('#sftp-upload-tbody').empty().append(Mustache.render( todoRowTemplate, {
 				strings: Strings,
                 files: files
         } ));
-        
+
         $('#sftp-upload-tbody tr').off().on('click', function(){
             var fullPath = $(this).attr('x-file');
             CommandManager.execute( Commands.FILE_OPEN, { fullPath: fullPath } );
         });
-        
+
         $('#sftp-upload-tbody .upload-button').off().on('click', function(e){
             uploadItem($(this).attr('x-file'), $(this).attr('r-file'));
             e.stopPropagation();
         });
-        
+
         $('#sftp-upload-tbody .skip-button').off().on('click', function(e){
             skipItem($(this).attr('x-file'));
             e.stopPropagation();
         });
-        
+
         if ( callback ) { callback(); }
 	}
-    
+
     // upload ONE file to the server
     function uploadItem(localPath, remotePath){
         var serverInfo = dataStorage.get('server_info');
         _nodeDomain.exec('upload', localPath, remotePath, serverInfo).fail(function(err){
             updateStatus(err);
-        });;
+        });
     }
-    
+
     function uploadDirectory(localPath, remotePath){
         var serverInfo = dataStorage.get('server_info');
         _nodeDomain.exec('uploadDirectory', localPath, remotePath, serverInfo).fail(function(err){
             updateStatus(err);
-        });;
+        });
     }
-    
+
     // upload all files in the panel to the server
     function uploadAllItems(){
         var serverInfo = dataStorage.get('server_info');
@@ -192,7 +192,7 @@ define( function( require, exports, module ) {
             updateStatus(err);
         });
     }
-    
+
     function skipItem(path) {
         var changedFiles = dataStorage.get('changed_files');
         $('#brackets-sftp-upload tr[x-file="'+path+'"]').remove();
@@ -201,23 +201,23 @@ define( function( require, exports, module ) {
             dataStorage.set('changed_files', changedFiles);
         }
     }
-    
+
     function skipAllItems(){
         $('#brackets-sftp-upload tr').remove();
         dataStorage.set('changed_files', {});
     }
-    
+
     function updateStatus(status){
         $('#brackets-sftp-upload .status-stab').text(status);
     }
-	
+
 	/**
 	 * Listen for save or refresh and look for todos when needed.
 	 */
 	function registerListeners() {
 		var $documentManager = $( DocumentManager ),
 			$projectManager = $( ProjectManager );
-		
+
 		// Listeners bound to Brackets modules.
 		$documentManager
 			.on( 'documentSaved.todo', function( event, document ) {
@@ -229,7 +229,7 @@ define( function( require, exports, module ) {
                 }
                 var projectUrl = ProjectManager.getProjectRoot().fullPath;
                 var serverInfo = dataStorage.get('server_info');
-                if(serverInfo.uploadOnSave){
+                if(serverInfo != null && serverInfo.uploadOnSave){
                     uploadItem(path, path.replace(projectUrl, ''));
                     return;
                 }
@@ -254,53 +254,53 @@ define( function( require, exports, module ) {
                         e.stopPropagation();
                     });
                 }
-                
-			} ); 
-		
+
+			} );
+
 	}
-	
+
 	// Register panel and setup event listeners.
 	AppInit.appReady( function() {
 		var panelHTML = Mustache.render( todoPanelTemplate, {
 				strings: Strings
 			} );
-		
+
 		// Create and cache todo panel.
-		PanelManager.createBottomPanel( 'bigeyex.bracketsSFTPUpload.panel', $( panelHTML ), 100 );
+		WorkspaceManager.createBottomPanel( 'bigeyex.bracketsSFTPUpload.panel', $( panelHTML ), 100 );
 		$todoPanel = $( '#brackets-sftp-upload' );
-		
+
 		// Close panel when close button is clicked.
 		$todoPanel
 			.on( 'click', '.close', function() {
 				enablePanel( false );
 			} );
-		
+
 		// Setup listeners.
 		registerListeners();
-		
+
 		// Add listener for toolbar icon..
 		$todoIcon.click( function() {
 			CommandManager.execute( COMMAND_ID );
 		} ).appendTo( '#main-toolbar .buttons' );
-        
+
         $todoPanel.on('click', '.btn-server-setup',function(){
             settingsDialog.showDialog();
         });
-        
+
         $todoPanel.on('click', '.btn-upload-all',function(){
             uploadAllItems();
         });
-		
+
         $todoPanel.on('click', '.btn-skip-all',function(){
             skipAllItems();
         });
-		
-		
+
+
 		// Enable extension if loaded last time.
 		if ( preferences.get( 'enabled' ) ) {
 			enablePanel( true );
 		}
-        
+
         $(_nodeDomain).on('uploading', function(err, msg){
             updateStatus('Uploading: '+msg);
         });
