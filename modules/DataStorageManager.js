8ddfcde7a7c110f86d8fa8e7a7da6ec08725e212
Mitchell Hentges <mitch9654@gmail.com>
2014-11-10 19:50:17 -0800

    Oops, ensures that spaces are tabs - like rest of project

--- modules/DataStorageManager.js
+++ modules/DataStorageManager.js
@@ -3,61 +3,61 @@ define( function( require, exports, module ) {
 
 	// Get dependencies.
 	var extensionUtils = brackets.getModule( 'utils/ExtensionUtils' ),
-        _                 = brackets.getModule("thirdparty/lodash"),
-        FileSystem        = brackets.getModule("filesystem/FileSystem"),
-        ProjectManager = brackets.getModule( 'project/ProjectManager' ),
-        FileUtils         = brackets.getModule("file/FileUtils");
+		_				 = brackets.getModule("thirdparty/lodash"),
+		FileSystem		= brackets.getModule("filesystem/FileSystem"),
+		ProjectManager = brackets.getModule( 'project/ProjectManager' ),
+		FileUtils		 = brackets.getModule("file/FileUtils");
 
 
 
-    var dataCache = {},
-        projectUrl ='',
-        self = this,
-        fileUri = module.uri.replace(/[^\/]*$/, '')+'../config.json';
+	var dataCache = {},
+		projectUrl ='',
+		self = this,
+		fileUri = module.uri.replace(/[^\/]*$/, '')+'../config.json';
 
 
-    FileUtils.readAsText(FileSystem.getFileForPath(fileUri)).done(function(text){
-        dataCache = JSON.parse(text);
-    })
-    .fail(function (errorCode) {
-        dataCache = {};
-        FileUtils.writeText(FileSystem.getFileForPath(fileUri), '{}');
-    });
+	FileUtils.readAsText(FileSystem.getFileForPath(fileUri)).done(function(text){
+		dataCache = JSON.parse(text);
+	})
+	.fail(function (errorCode) {
+		dataCache = {};
+		FileUtils.writeText(FileSystem.getFileForPath(fileUri), '{}');
+	});
 
-    $(ProjectManager).on('projectOpen', function(){
-        projectUrl = ProjectManager.getProjectRoot().fullPath;
-    });
+	$(ProjectManager).on('projectOpen', function(){
+		projectUrl = ProjectManager.getProjectRoot().fullPath;
+	});
 
-    function init(callback){
+	function init(callback){
 
 
-    }
+	}
 
-    function get(key){
-        if(!(projectUrl in dataCache)){
-            dataCache[projectUrl] = {};
-        }
-        if(key in dataCache[projectUrl]){
-            return dataCache[projectUrl][key];
-        }
-        else{
-            return null;
-        }
-    }
+	function get(key){
+		if(!(projectUrl in dataCache)){
+			dataCache[projectUrl] = {};
+		}
+		if(key in dataCache[projectUrl]){
+			return dataCache[projectUrl][key];
+		}
+		else{
+			return null;
+		}
+	}
 
-    function set(key, value){
-        if(!(projectUrl in dataCache)){
-            dataCache[projectUrl] = {};
-        }
-        dataCache[projectUrl][key] = value;
-        _save();
-    }
+	function set(key, value){
+		if(!(projectUrl in dataCache)){
+			dataCache[projectUrl] = {};
+		}
+		dataCache[projectUrl][key] = value;
+		_save();
+	}
 
-    function _save(){
-        FileUtils.writeText(FileSystem.getFileForPath(fileUri), JSON.stringify(dataCache));
-    }
+	function _save(){
+		FileUtils.writeText(FileSystem.getFileForPath(fileUri), JSON.stringify(dataCache));
+	}
 
-    exports.get = get;
-    exports.set = set;
+	exports.get = get;
+	exports.set = set;
 
 } );
