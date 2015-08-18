56606e9b58e471bb5058b4a9f40f55695dda13d8
Michal Matlega <m1sz3lek@gmail.com>
2015-08-18 18:17:57 +0200

    Polish translation

---
+++ nls/pl/Strings.js
@@ -0,0 +1,34 @@
+define( {
+	// EXTENSION.
+	EXTENSION_NAME: "SFTP Upload",
+    
+    // MENUS
+    UPLOAD_MENU_NAME: "Wyślij przez SFTP",
+    DOWNLOAD_MENU_NAME: "Ściągnij z serwera",
+	
+	// GENERAL.
+	YES:    "Tak",
+	NO:     "Nie",
+	OK:     "Ok",
+	CANCEL: "Anuluj",
+	UPLOAD: "Wyślij",
+	SKIP:   "Pomiń",
+	
+	// TOOLBAR.
+	SERVER_SETUP: "Ustawienia serwera",
+	UPLOAD_ALL: "Wyślij wszystko",
+	SKIP_ALL: "Pomiń wszystko",
+	
+	// SETTINGS DIALOG.
+	SETTINGS_DIALOG_TITLE:        "Ustawienia SFTP",
+	SETTINGS_DIALOG_TYPE:    	  "Typ",
+	SETTINGS_DIALOG_TYPE_FTP: 	  "FTP",
+	SETTINGS_DIALOG_TYPE_SFTP:    "Sftp(SSH)",
+	SETTINGS_DIALOG_HOST: 		  "Host",
+	SETTINGS_DIALOG_PORT: 		  "Port",
+	SETTINGS_DIALOG_USERNAME: 	  "Nazwa użytkownika",
+	SETTINGS_DIALOG_PASSWORD: 	  "Hasło",
+    SETTINGS_DIALOG_RSAMSG:       "Ścieżka klucza RSA",
+	SETTINGS_DIALOG_PATH: 		  "Ścieżka serwera",
+    SETTINGS_UPLOAD_ON_SAVE:      "Wysyłaj przy zapisie"
+} );
